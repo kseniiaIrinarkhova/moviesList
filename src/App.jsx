29fc5c09f919a6074ed728cd8b6fac1a92c03e30
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-03-07 15:08:34 -0800

    change fetch to axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-import { useState } from 'react'
+//additional library
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
+//components
+import Form from './components/Form/Form';
+import MovieDisplay from './components/MovieDisplay/MovieDisplay';
+
+//style
 import './App.css'
 
+//constant variables
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 function App() {
-  
+
+  //state to hold movie data
+const[movieData,setMovieData] = useState(null);
+
+async function search(query){
+  try{
+  // Make get requestand store the response
+  const response = await axios.get(
+    `http://www.omdbapi.com/?apikey=${API_KEY}&t=${query}`
+  );
+
+  // Set the Movie state to the received data
+    setMovieData(response);
+    console.log(response)
+  }catch(err){
+    console.log(err)
+  }
+}
 
   return (
     <>
-      <h1>{import.meta.env.VITE_TEST}</h1>
+      <div className="App">
+        <Form searchMovie={getMovieData}/>
+        {movieData ? <MovieDisplay /> :null}
+      </div>
     </>
   )
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,23 @@
 
-function Form() {
+import { useState } from "react"
+
+function Form({searchMovie}) {
+//State to hold the data of our form
+const[formData, setFormData] = useState({query : ""});
+
+//handle change on input field
+function handleChange(e){
+  let newFormData = {...formData};
+  newFormData[e.target.name] = e.target.value
+  setFormData(newFormData);
+}
+
+
   return (
-    <div>Form</div>
+    <form >
+        <input type="search" placeholder="Movie title..." />
+        <input type="submit" value="Search" />
+    </form>
   )
 }
 
